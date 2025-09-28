7b217be0bec3de382f6ed72867a7283b7f27d565
DJPetika <DJPetika@gmail.com>
2025-09-28 15:08:38 -0500

    added creting and editting events. Events now show up on the index page as well.

--- app/controllers/activity_events_controller.rb
+++ app/controllers/activity_events_controller.rb
@@ -3,20 +3,49 @@ class ActivityEventsController < ApplicationController
 
   # GET /activity_events or /activity_events.json
   def index
-    @activity_events = ActivityEvent.all
+    # Get the current date
+    today = Date.current
+
+    # Calculate the date of the Monday of the current week
+    # Date.current.beginning_of_week defaults to Monday as the start of the week.
+    this_monday = today.beginning_of_week
+
+    @activity_events_Mon_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday }
+    @activity_events_Tue_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday + 1.day }
+    @activity_events_Wed_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday + 2.day }
+    @activity_events_Thr_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday + 3.day }
+    @activity_events_Fri_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday + 4.day }
+    @activity_events_Sat_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday + 5.day }
+    @activity_events_Sun_TD = ActivityEvent.select{ |event| event.is_completed == false && event.due_date == this_monday + 6.day }
+
+    @activity_events_Mon_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday }
+    @activity_events_Tue_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday + 1.day }
+    @activity_events_Wed_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday + 2.day }
+    @activity_events_Thr_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday + 3.day }
+    @activity_events_Fri_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday + 4.day }
+    @activity_events_Sat_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday + 5.day }
+    @activity_events_Sun_Dn = ActivityEvent.select{ |event| event.is_completed == true && event.due_date == this_monday + 6.day }
   end
 
   # GET /activity_events/1 or /activity_events/1.json
   def show
+    @activity = Activity.find{ |activity| activity.id == @activity_event.activity_id }
+    @activity_name = @activity.ActivityName
+    @user= User.find{ |user| user.id == @activity_event.user_id }
+    @user_name = @user.name
   end
 
   # GET /activity_events/new
   def new
     @activity_event = ActivityEvent.new
+    @users_for_dropdown = User.all.map { |user| [user.name, user.id] } # Or any other desired attributes
+    @activities_for_dropdown = Activity.all.map { |activity| [activity.ActivityName, activity.id] } # Or any other desired attributes
   end
 
   # GET /activity_events/1/edit
   def edit
+    @users_for_dropdown = User.all.map { |user| [user.name, user.id] } # Or any other desired attributes
+    @activities_for_dropdown = Activity.all.map { |activity| [activity.ActivityName, activity.id] } # Or any other desired attributes
   end
 
   # POST /activity_events or /activity_events.json
